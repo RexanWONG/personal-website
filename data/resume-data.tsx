ec0277d6267aa686a86505e0c5420e57901c93f0
RexanWONG <96183717+RexanWONG@users.noreply.github.com>
2024-01-24 22:09:21 +0800

    add a couple of more awards

--- data/resume-data.tsx
+++ data/resume-data.tsx
@@ -9,7 +9,7 @@ export const RESUME_DATA = {
   about:
     "16 year old high schooler and solopreneur building internet startups.  5x Web3 hackathon winner.",
   summary:
-    "My summary",
+    "Very driven, passionate, fun individual.",
   avatarUrl: "https://avatars.githubusercontent.com/u/96183717?v=4",
   personalWebsiteUrl: "https://rexanwong.me",
   contact: {
@@ -40,9 +40,39 @@ export const RESUME_DATA = {
       title: "Top 10 Finalistis (Winning team) + Mantle Best UX Award",
       logo: EthGlobalLogo,
       date: "10/2023",
-      description: "I built Koinu, an npm library that allows developers to include a cross chain experience within their dapps.  This project was selected as a finalist project, along with 9 other finalist projects, out of 500 projects.  Being the only under-18 to win the competition, and competiting against the best and most hardcore developers in the Web3 space, out of 515 projects, I learnt a lot more about Web3 and blockchain development during the course of the hackathon and it encouraged me to build blockchain applications more : the fact that I could compete, as a 15 year old, at the time, against the top developers of the Web3 space, and win, I am looking forward to build more applications for everyday users of Web3",
+      description: "ETHOnline is a three week long online hackathon with over $200,000 USD in prizes as well as a series of curated summits, celebrating some of the most significant happenings & learnings from the past 12 months in web3.  The hackathon is focused on building Ethereum based applications with different bounties' tools and technologies.  I built Koinu, an npm library that allows developers to include a cross chain experience within their dapps.  This project was selected as a finalist project, along with 9 other finalist projects, out of 500 projects.  Being the only under-18 to win the competition, and competiting against the best and most hardcore developers in the Web3 space, out of 515 projects, I learnt a lot more about Web3 and blockchain development during the course of the hackathon and it encouraged me to build blockchain applications more : the fact that I could compete, as a 15 year old, at the time, against the top developers of the Web3 space, and win, I am looking forward to build more applications for everyday users of Web3",
       projectDetailsLink: "https://ethglobal.com/showcase/koinu-x8ced",
-    }
+    },
+    {
+      name: "Not Another Virtual Hackathon (NAVH) by Cosensys",
+      link: "https://navhackathon.consensys.io/",
+      badges: ["Hackathon", "Online", "70+ Projects"],
+      title: "Winner of the IYKYK Linea Edition",
+      logo: EthGlobalLogo,
+      date: "9/2023",
+      description: "Consensys, one of the biggest blockchain companies, hosted NAVH: Not Another Virtual Hackathon beginning July 6th, 2023, for technical and non-technical builders to bring their skills to the table. Builders from anywhere in the world can join to compete for over $115,500 in bounties, using Cosensys products to build something.  I built Proof of Workout Protocol : a dApp on Linea (a blockchain developed by leading blockchain company Cosensys) designed to motivate and reward physical fitness through a gamified system of quests and token-based incentive.  ",
+      projectDetailsLink: "https://github.com/rexanwong/proof-of-workout-protocol",
+    },
+    {
+      name: "Developer Dao & Mina Protocol Hackathon",
+      link: "https://twitter.com/rexan_wong/status/1681156987143462912",
+      badges: ["Hackathon", "Online", "40+ Projects"],
+      title: "1st Place",
+      logo: EthGlobalLogo,
+      date: "6/2023",
+      description: "Developer DAO, the biggest Web3 Developer Organisation, and Mina Protocol, a zero knowledge based blockchain with a market cap of $800 million USD, ran a week-long, remote hackathon with ~ $10,000 in prizes to be won in USDC, Mina Token and CODE.  The hackathon is focused on building zero-knowledge applications on Mina Protocol with SnarkyJS.   I built a privacy-focused decentralized zkApp that aims to enable anonymous and secure protests where participants can join and support various causes while preserving their privacy.  This hackathon was the most hardcore hackathon I've participated in : joining the hackathon, knowing very little about zero-knowledge (zk) technology, let alone building zk technology.  Throughout the process, I had to dive deep and learn all these new concepts while building my zkApp.  In the end, my project won first place in the hackathon, out of 40 other projects, and this victory provoked me to go further deeper into this tough concept of zero knowledge technology.",
+      projectDetailsLink: "https://github.com/rexanwong/zk-protest",
+    },
+    {
+      name: "Apple Swift Student Challenge",
+      link: "https://twitter.com/rexan_wong/status/1656085283836940288",
+      badges: ["Hackathon", "Online", "13500+ Submissions"],
+      title: "WWDC23 Scholar",
+      logo: EthGlobalLogo,
+      date: "Q2 2023",
+      description: "The Swift Student Challenge at WWDC (Worldwide Developers Conference) is an annual competition organized by Apple to celebrate the ingenuity and expertise of student developers using Swift.  Apple picks around 300 applications out of a pool of more than 10,000+ applicants to win, and I was one of the 300 applicants to win the challenge.   I built an iOS app/game that challenges users to find an image of a random word generated by the app in their photo album, then used machine learning models to verify that the image being selected matches the random word.",
+      projectDetailsLink: "https://github.com/rexanwong/photorush",
+    },
   ],
   work: [
     {
